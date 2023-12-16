3659b39fce39231de90c42e464bdd4ee4789f37b
Clark Ngo <30854108+clarkngo@users.noreply.github.com>
2023-12-17 00:53:38 +0800

    Create test_hello.py

---
+++ test_hello.py
@@ -0,0 +1,15 @@
+import unittest
+from app import app 
+
+class TestHelloWorld(unittest.TestCase):
+
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_hello_world(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Hello, World!', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
