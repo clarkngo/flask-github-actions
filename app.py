f172dbda8f59a37b1c88c8c7c001d022669789ed
Clark Ngo <30854108+clarkngo@users.noreply.github.com>
2024-05-02 22:55:40 +0000

    test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app.config['TESTING'] = True
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Flask GitHub Actions!</p>"
+    return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
